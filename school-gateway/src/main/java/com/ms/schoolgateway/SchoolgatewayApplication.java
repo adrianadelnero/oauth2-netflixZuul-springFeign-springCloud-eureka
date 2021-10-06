98875f34c70cc3d74bc54a75b6821aa6561a3c63
Adriana Del Nero Romano <adriana.delnero@gmail.com>
2021-10-05 23:03:11 -0300

    Alterações oauth

--- school-gateway/src/main/java/com/ms/schoolgateway/SchoolgatewayApplication.java
+++ school-gateway/src/main/java/com/ms/schoolgateway/SchoolgatewayApplication.java
@@ -3,13 +3,24 @@ package com.ms.schoolgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class SchoolgatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchoolgatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(SchoolgatewayApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 }
--- school-gateway/src/test/java/com/ms/schoolgateway/SchoolgatewayApplicationTests.java
+++ school-gateway/src/test/java/com/ms/schoolgateway/SchoolgatewayApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SchoolgatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
